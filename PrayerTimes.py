f79c0ae74c797c8dd9e095b2c1a88030a17efb6d
Haseeb Iqbal <hi516@ic.ac.uk>
2020-09-06 16:35:24 +0100

    Adapted it to remove personal preferences

--- PrayerTimes.py
+++ PrayerTimes.py
@@ -8,16 +8,15 @@ import random, requests
 import arabic_reshaper
 from bidi.algorithm import get_display
 from math import *
+import http.client
 
 root = Tk()
 root.title("Prayer Times")
 root.geometry("1920x1080")
 
-titlelabel = Label(root, text="Prayer Times (Crawley)")
+titlelabel = Label(root, text="Prayer Times (London Unified Timetable)")
 titlelabel.config(font=("Calibri 26 underline"))
-titlelabel.place(relx=0.2, y=25, anchor=CENTER)
-
-
+titlelabel.place(relx=0.5, y=25, anchor=CENTER)
 
 def get_times():
 	''' Gets times from the mosque website as a list and formats each item as a datetime object'''
@@ -49,73 +48,68 @@ def refresh():
 	'''refreshes the UI once the current time reaches the Salah time'''
 	now = datetime.now()
 	time_now = datetime.time(now)
-	curr_timelabel = Label(root, text="Current Time: " + str(time_now))
-	curr_timelabel.place(relx=0.2, y=75, anchor=CENTER)
+	curr_timelabel = Label(root, text="Current Time: " + str(time_now)[:8])
+	curr_timelabel.config(font=("Calibri 16"))
+	curr_timelabel.place(relx=0.5, rely=0.065, anchor=CENTER)
 
 	if time_now > times[0] and time_now < times[1]:
-		salah = "fajr"
 		curr_salahlabel = Label(root, text="Current Salah: Fajr")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 
 		next_salahlabel = Label(root, text="Sunrise: " + times[1])
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	elif time_now > times[1] and time_now < times[2]:
-		salah = "sunrise"
 		curr_salahlabel = Label(root, text="Sunrise - No Salah")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 
 		next_salahlabel = Label(root, text="Next Salah: Zuhr " + "(" + str(times[2]) + ")")
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	elif time_now > times[2] and time_now < times[3]:
-		salah = "zuhr"
 		curr_salahlabel = Label(root, text="Current Salah: Zuhr")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 
 		next_salahlabel = Label(root, text="Next Salah: Asr " + "(" + str(times[3]) + ")")
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	elif time_now > times[3] and time_now < times[4]:
-		salah = "asr"
 		curr_salahlabel = Label(root, text="Current Salah: Asr")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 
 		next_salahlabel = Label(root, text="Next Salah: Maghrib " + "(" + str(times[4]) + ")")
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	elif time_now > times[4] and time_now < times[5]:
-		salah = "maghrib"
 		curr_salahlabel = Label(root, text="Current Salah: Maghrib")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 
 		next_salahlabel = Label(root, text="Next Salah: Isha " + "(" + str(times[5]) + ")")
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	elif time_now > times[5]:
-		salah = "isha"
 		curr_salahlabel = Label(root, text="Current Salah: Isha")
 		curr_salahlabel.config(font=("Calibri, 18"))
-		curr_salahlabel.place(relx=0.1, y=220, anchor=CENTER)
+		curr_salahlabel.place(relx=0.1, rely=0.29, anchor=CENTER)
 		
 		next_salahlabel = Label(root, text="Next Salah: Fajr " + "(" + str(times[0]) + "~)")
 		next_salahlabel.config(font=("Calibri, 18"))
-		next_salahlabel.place(relx=0.3, y=220, anchor=CENTER)
+		next_salahlabel.place(relx=0.3, rely=0.29, anchor=CENTER)
 
 
 	root.after(1000, refresh)
@@ -123,6 +117,39 @@ def refresh():
 	root.after(1000, next_salahlabel.pack_forget)
 	root.after(1000, curr_timelabel.pack_forget)
 
+def all_salah():
+	fajrlabel = Label(root, text="Fajr: " + times_list[0])
+	fajrlabel.config(font=("Calibri, 18"))
+	fajrlabel.place(relx=0.5, rely=0.29, anchor=CENTER)
+
+	sunriselabel = Label(root, text="Sunrise: " + times_list[1])
+	sunriselabel.config(font=("Calibri, 18"))
+	sunriselabel.place(relx=0.5, rely=0.33, anchor=CENTER)
+
+	zuhrlabel = Label(root, text="Zuhr: " + times_list[2])
+	zuhrlabel.config(font=("Calibri, 18"))
+	zuhrlabel.place(relx=0.5, rely=0.37, anchor=CENTER)
+
+	asrlabel = Label(root, text="Asr: " + times_list[3])
+	asrlabel.config(font=("Calibri, 18"))
+	asrlabel.place(relx=0.5, rely=0.41, anchor=CENTER)
+
+	maghriblabel = Label(root, text="Maghrib: " + times_list[4])
+	maghriblabel.config(font=("Calibri, 18"))
+	maghriblabel.place(relx=0.5, rely=0.45, anchor=CENTER)
+
+	ishalabel = Label(root, text="Isha: " + times_list[5])
+	ishalabel.config(font=("Calibri, 18"))
+	ishalabel.place(relx=0.5, rely=0.49, anchor=CENTER)
+
+all_salah()
+
+def masjidinfo():
+	masjidinfolabel = Label(root, text="Masjid information")
+	masjidinfolabel.config(font=("Calibri 18 underline"))
+	masjidinfolabel.place(relx=0.87, rely=0.062, anchor=CENTER)
+
+masjidinfo()
 
 
 def Clock0(w, nx, ny):                                  # clock draw function
@@ -175,21 +202,19 @@ def Clock0(w, nx, ny):                                  # clock draw function
 	y = y0 - r2 * cos(phi)
 	w.create_line(x0, y0 , x, y, arrow=LAST)                # draw second hand
 
-	root.after(60000, Clock0)
-
 def Clock(w, nx, ny):                               # clock callback function
 	w.delete(ALL)                                              # delete canvas
 	Clock0(w, nx, ny)                                             # draw clock
+	#print("updating clock...")
 	w.after(10, Clock, w, nx, ny)                  # call callback after 10 ms
-
-	root.after(60000, Clock)
+	
 
 # main
 
 
 nx = 200; ny = 200                                              # canvas size
 w = Canvas(root, width=nx, height=ny)         # create canvas w
-w.place(relx=0.6, rely=0.7)                                                # make canvas visible
+w.place(relx=0.1, rely=0.4, anchor=CENTER)                                                # make canvas visible
 
 Clock(w, nx, ny)                                        # call clock function
 
@@ -197,8 +222,8 @@ Clock(w, nx, ny)                                        # call clock function
 def bring_verse(verse):
 	ayahlabel = Label(root, text="Ayah of the Day")
 	ayahlabel.config(font=("Calibri 15 underline"))
-	ayahlabel.place(relx=0.2, rely=0.5, anchor=CENTER)
-	url = 'http://api.alquran.cloud/ayah/'+str(verse)+'/editions/quran-uthmani,en.pickthall'
+	ayahlabel.place(relx=0.5, rely=0.6, anchor=CENTER)
+	url = 'http://api.alquran.cloud/ayah/'+str(verse)+'/editions/quran-uthmani,en.asad'
 	json_data = requests.get(url).json()
 	verse_a = json_data['data'][0]['text']
 	verse_en = json_data['data'][1]['text']
@@ -214,127 +239,33 @@ def ayah():
 	reshaped_text = arabic_reshaper.reshape(arabic)
 	bidi_text = get_display(reshaped_text)
 	newlabel = Label(root, text=bidi_text)
-	newlabel.place(relx=0.2, rely=0.53, anchor=CENTER)
+	newlabel.place(relx=0.5, rely=0.63, anchor=CENTER)
 	englishlabel = Label(root, text=link[1], wraplength=700)
-	englishlabel.place(relx=0.2, rely=0.57, anchor=CENTER)
+	englishlabel.place(relx=0.5, rely=0.67, anchor=CENTER)
 	reflabel = Label(root, text=link[2])
-	reflabel.place(relx=0.2, rely=0.60, anchor=CENTER)
+	reflabel.place(relx=0.5, rely=0.7, anchor=CENTER)
 	
 ayah()
 
 def hadith():
 	hadithlabel = Label(root, text="Hadith of the Day")
 	hadithlabel.config(font=("Calibri 15 underline"))
-	hadithlabel.place(relx=0.2, rely=0.67, anchor=CENTER)
+	hadithlabel.place(relx=0.5, rely=0.77, anchor=CENTER)
+	# conn = http.client.HTTPSConnection("api.sunnah.com")
+
+	# payload = "{}"
+
+	# headers = { 'x-api-key': "SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk" }
+
+	# conn.request("GET", "/v1/hadiths/random", payload, headers)
+
+	# res = conn.getresponse()
+	# print(res.read())
 
 hadith()
 
-def weathercrawley():
-	weathercrawleylabel = Label(root, text="Weather (Crawley)")
-	weathercrawleylabel.config(font=("Calibri 26 underline"))
-	weathercrawleylabel.place(relx=0.57, y=25, anchor=CENTER)
-	urlbbcc = 'https://www.bbc.co.uk/weather/2652053'
-	rc = requests.get(urlbbcc)
-	soupc = BeautifulSoup(rc.text, features="html.parser")
-
-	weatherclist = []
-	for headline in soupc.find_all(class_="wr-day__weather-type-description wr-js-day-content-weather-type-description wr-day__content__weather-type-description--opaque"):
-	    weatherclist.append(headline.contents[0].strip())
-	todayc = weatherclist[0]
-	todayclabel = Label(root, text=todayc)
-	todayclabel.config(font=("Calibri, 14"))
-	todayclabel.place(relx=0.57, y=90, anchor=CENTER)
-
-	temps = []
-	for temp in soupc.find_all(class_="wr-value--temperature--c"):
-		temps.append(str(temp))
-	temps_new = [temps[0], temps[1]]
-	print(temps_new)
-	temps_final = []
-	for i in temps_new:
-		for x in i:
-			if x.isnumeric():
-				temps_final.append(x)
-	if len(temps_final) == 3:
-		final = [temps_final[0] + temps_final[1], temps_final[2]]
-	elif len(temps_final) == 4:
-		final = [temps_final[0] + temps_final[1], temps_final[2] + temps_final[3]]
-	else:
-		final = [temps_final[0], temps_final[1]]
-	print(final)
-	highlabel = Label(root, text="Highest temp = " + final[0])
-	highlabel.config(font=("Calibri 12"))
-	highlabel.place(relx=0.57, y=125, anchor=CENTER)
-	lowlabel = Label(root, text="Lowest temp = " + final[1])
-	lowlabel.config(font=("Calibri 12"))
-	lowlabel.place(relx=0.57, y=155, anchor=CENTER)
-
-weathercrawley()
-
-def weatherlondon():
-	weatherlondonlabel = Label(root, text="Weather (London)")
-	weatherlondonlabel.config(font=("Calibri 26 underline"))
-	weatherlondonlabel.place(relx=0.8, y=25, anchor=CENTER)
-	urlbbcl = 'https://www.bbc.co.uk/weather/2643743'
-	rl = requests.get(urlbbcl)
-	soupl = BeautifulSoup(rl.text, features="html.parser")
-
-	weatherllist = []
-	for headline in soupl.find_all(class_="wr-day__weather-type-description wr-js-day-content-weather-type-description wr-day__content__weather-type-description--opaque"):
-	    weatherllist.append(headline.contents[0].strip())
-	todayl = weatherllist[0]
-	todayllabel = Label(root, text=todayl)
-	todayllabel.config(font=("Calibri, 14"))
-	todayllabel.place(relx=0.8, y=90, anchor=CENTER)
-
-	temps = []
-	for temp in soupl.find_all(class_="wr-value--temperature--c"):
-		temps.append(str(temp))
-
-	temps_new = [temps[0], temps[1]]
-
-	temps_final = []
-	for i in temps_new:
-		for x in i:
-			if x.isdigit():
-				temps_final.append(x)
-
-	if len(temps_final) == 3:
-		final = [temps_final[0] + temps_final[1], temps_final[2]]
-	elif len(temps_final) == 4:
-		final = [temps_final[0] + temps_final[1], temps_final[2] + temps_final[3]]
-	else:
-		final = [temps_final[0], temps_final[1]]
-
-	highlabel = Label(root, text="Highest temp = " + final[0])
-	highlabel.config(font=("Calibri 12"))
-	highlabel.place(relx=0.8, y=125, anchor=CENTER)
-	lowlabel = Label(root, text="Lowest temp = " + final[1])
-	lowlabel.config(font=("Calibri 12"))
-	lowlabel.place(relx=0.8, y=155, anchor=CENTER)
-
-weatherlondon()
-
-def tasks():
-	tasks = requests.get(
-    "https://api.todoist.com/rest/v1/tasks",
-	params={
-        "filter": "Today"
-    },
-    headers={
-        "Authorization": "Bearer 1426994c24642fa2552d2436e628e6e909ad03d1"
-    }).json()
-
-	print(tasks)
-	tasks_formatted = []
-
-	for i in tasks:
-		tasks_formatted.append(i["content"])
-	print(tasks_formatted)
-
-tasks()
-
 root.after(1000, refresh)
-root.after(60000, Clock0)
-root.after(60000, Clock)
 root.mainloop()
+
+
+
